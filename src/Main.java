18e42d5a246f748336ceb976ac93e14398994ca9
herreang000 <herreang000@warren.k12.in.us>
2024-08-30 11:07:29 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+
+
+    }
+
+    private static boolean isCatPlaying(boolean isSummer, int temperature){
+        if(temperature >= 25 && temperature <= 45){
+            return true;
+        }else if(temperature >= 25 && temperature <= 35){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+
+
+
 }
